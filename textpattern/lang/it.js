0685c45d9a07fb769e513c54e93c49cfdb1ab036
Luca Sabato <sabatia@gmail.com>
2009-05-26 20:53:20 +0200

    aggiunto i file del date picker

---
+++ textpattern/lang/it.js
@@ -0,0 +1,8 @@
+var fdLocale = {
+                fullMonths:["Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Otobre","Novembre","Dicembre"],
+                monthAbbrs:["Gen","Feb","Mar","Apr","Mag","Giu","Lug","Ago","Set","Ott","Nov","Dic"],
+                fullDays:  ["Lunedi","Martedi","Mercoledi","Giovedi","Venerdi","Sabato","Domenica"],
+                dayAbbrs:  ["Lun","MAr","Mer","Gio","Ven","Sab","Dom"],
+                titles:    ["Mese Precedente","Mese successivo","Anno Precedente","Anno prossimo", "Oggi", "Visualizza calendario", "set", "Settimana [[%0%]] di [[%1%]]", "Settimana", "Seleziona una data", "Clicca \u0026 e trascina", "Visualizza \u201C[[%0%]]\u201D il primo", "Vai alla data di oggi\u2019s", "Disabilita la data"],
+                firstDayOfWeek:0
+};
